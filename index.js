cc66a90bcbfd728f2b81ac1723c13795296e5830
Iqra Jawed <>
2024-05-27 09:47:53 +0500

    new file added

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const age = 20;
 const name = "iqra";
+
 const dob = '05/05/2021';
 
 function myfunc(){
     console.log("My name is " + name);
 } 
+
+
+function getDate(){
+    return dob;
+}
+
+function ageFactor(){
+    return ageFactor >= 20;
+}
+
+
